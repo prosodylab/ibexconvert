e15ccfa130374bc9bfe901f7cad9e527bb3cd080
Theodore Morley <theodore.morley@mail.mcgill.ca>
2016-06-06 10:50:22 -0400

    Adds support for audio and screening in the combination script

--- combinePlus.py
+++ combinePlus.py
@@ -1,5 +1,7 @@
 #PLAN: take two input files, and an output name(3 args). The first input is the original experiment file, the second is
 #the experiment results.
+#plan to adjust new  stuff:
+#If controllertype=form, store the rest of the cells in the row in special screeningData structure of some kind?
 import sys
 import re
 import json
@@ -19,7 +21,7 @@ trialDataList=re.split(r"(?:\r\n)|(?:\n)|(?:\r)",f2.read())
 
 f2.close()
 #nonHeadersString=''.join(experimentData[1:len(experimentData)])#old code, possibly useful later
-#attempting to grab the trial headers
+#grabs the trial headers
 trialDataHeadersA=[]
 pattern=re.compile(r"(?:^#\s*Col\.\s*)(\d?\d?)(?::)(?:\s*)(.*)")
 for trialHeadA in trialDataList:
@@ -56,7 +58,8 @@ for workerLines in trialDataList:
                 workerCount+=1
 #retrieve headers from original files
 headersString=experimentData[0]+trialString+"\tWorker ID Number"
-
+#read number of tabs in experimentfile, to add form data without having odd number of columns
+tabcount=experimentData[1].count('\t')
 #open output
 newoutput=open(outputFileName,'w')
 #write headers to newoutput
@@ -64,11 +67,23 @@ newoutput.write(headersString+'\n')
 #check item number(4th in each row from trial data) with row number of originalExperiment
 workerCount2=0
 qNumCount2=0
+screeningPattern=re.compile(r"^.*,.*,(Form).*$")
 for trialLine in trialDataList:
     if len(trialLine)>0 and trialLine[0]!="#":
+        match=screeningPattern.match(trialLine)
         itemNum=int(trialLine.split(',')[3])-2
-        if itemNum and itemNum>0:
-            #take row number item num from experimentData
-            #add workerid based on experiment type
-            newoutput.write(experimentData[itemNum]+"\t"+trialLine.replace(",", "\t")+"\t""\n")
+        if match:
+            #place all in array and put at end? Or individual columns for each piece of info? Easy from this point
+            formGatherA=trialLine.split(',')
+            t=-1
+            while t<tabcount:
+                t=t+1
+                newoutput.write("NULL\t")
+            newoutput.write(trialLine.replace(",","\t")+"\t""\n")
+            #if itemNum and itemNum>0:
+        else:
+            if itemNum and itemNum>0:
+                #take row number item num from experimentData
+                #add workerid based on experiment type
+                newoutput.write(experimentData[itemNum]+"\t"+trialLine.replace(",", "\t")+"\t""\n")
 newoutput.close()
