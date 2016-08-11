2d41b1877d8db6d96626710c490eb83ad0823a54
Theodore Morley <theodore.morley@mail.mcgill.ca>
2016-08-10 21:07:32 -0400

    Updated annotation script for Self Paced Reading.

--- annotate2.py
+++ annotate2.py
@@ -54,6 +54,7 @@ def WOI_Annotation(Exp,Res,Out):
                 ResRowList.append(ResLine)
     ResHeaderString=','.join(ResHeaderList)
     commaCount=ResHeaderString.count(',')
+    print commaCount
     ResRowList.insert(0,ResHeaderString)
 
     #place column full of NA and headed Word of Interest
@@ -75,6 +76,7 @@ def WOI_Annotation(Exp,Res,Out):
     expPattern=re.compile(r"^(.*)_(.*)$")#(r"^(\w*.?,?)_(.*)$")
     expPattern2=re.compile(r"^(.*)$")#(r"^(\w*.?,?)$")
     resCondPattern=re.compile(r"0-(\d)")
+    condPattern2=re.compile(r"(.*)-(\d)")
     for expR in ExpLines:
         #extracting experiment condition and item number(matches item in the Res to the row num in the Exp)
         expItem=indexwd(expR,ColNames,"item")#ExpLines.index(expR)
@@ -97,22 +99,28 @@ def WOI_Annotation(Exp,Res,Out):
                 wordArray.append(expMatch2.group(1))
                 markerArray.append("NA")
         resInd=0
-        for resR in ResRowList:
+        for i,resR in enumerate(ResRowList):
             resItem="!"
             if indexwd(resR,ResHeaderList,"Group.").isdigit():
-                resItem=int(indexwd(resR,ResHeaderList,"Group."))/2
+                resItem=int(indexwd(resR,ResHeaderList,"Group."))
             resCond=""
             if indexwd(resR,ResHeaderList,"Type.")!=None:
                 resCond=indexwd(resR,ResHeaderList,"Type.")
             rCond=-1
-            if str(resInd)<=str(ResRowList.index(resR)):
+            if resInd<=i:
                 sentenceFinished=False
-            resMatch=resCondPattern.match(resCond)
+            resMatch=condPattern2.match(resCond)
             if resMatch:
-                rCond=resMatch.group(1)
+                if resMatch.group(1)=="filler":
+                    #filler
+                    resItem=resMatch.group(2)
+                    rCond=resMatch.group(1)
+                else:
+                    #nonfiller
+                    resItem=resMatch.group(1)
+                    rCond=resMatch.group(2)
             #need to extract condition and item number from both
             #need to match based on condition and item number(adjusted by some factor??)
-            #need to track where I am in sentence, and reset each time we reach a new one(variable:senPos)
             #if sentence/item/cond matches then start a while loop
             #print resItem+"=="+expItem
             #print str(rCond)+"=="+str(expCond)
@@ -120,11 +128,15 @@ def WOI_Annotation(Exp,Res,Out):
                 for idx, val in enumerate(wordArray):
                     #goals: using idx, check each word against the experiment file and mark woi by appending to ResRowList
                     #how to:
+                    print resR[-1]
                     if resR[-1]=="NULL" or (resInd)>len(ResRowList) or resR[-1].isdigit():
                         resInd+=1
                         break
                     #issue:resInd is off in some cases
-                    if val==indexwd(ResRowList[resInd],ResHeaderList,"Field value./Word./Answer.").replace("%2C",","):
+                    #print str(idx)+"??"+str(resInd)
+                    #print val+"=="+indexwd(ResRowList[resInd],ResHeaderList,"Field value./Answer./Word.").replace("%2C",",")
+                    if val==indexwd(ResRowList[resInd],ResHeaderList,"Field value./Answer./Word.").replace("%2C",","):
+                        #print "replacing 2c chars"
                         if ResRowList[resInd][10]=="NA":
                             ResRowList[resInd][10]=markerArray[idx]
                             ResRowList[resInd][11]=markerArray[idx]
@@ -146,7 +158,7 @@ def WOI_Annotation(Exp,Res,Out):
         #if not NA, change currrent zoi
         if ResRowList[idx][11]=="Zone of Interest":
             break
-        if str(indexwd(r,ResHeaderList,"Field name./Question2 (NULL if none)./Word number./Question (NULL if none)."))=="1":
+        if str(indexwd(r,ResHeaderList,"Field name./Question2 (NULL if none)./Question (NULL if none)./Word number."))=="1":
             if indexwd(r,ResHeaderList,"Zone of Interest")=="NA":
                 ResRowList[idx][11]=currentZoi
             currentZoi="NA"
@@ -164,14 +176,15 @@ def WOI_Annotation(Exp,Res,Out):
         if idx==0:
             pass
         elif idx!=0 and "workerid" in row:
-            currentID=indexwd(row,ResHeaderList,"Field value./Word./Answer.")
+            currentID=indexwd(row,ResHeaderList,"Field value./Answer./Word.")
             if currentID=="":
                 currentID="NULL"
             ResRowList[idx].append(currentID)
         else:
             ResRowList[idx].append(currentID)
     ###ORIGINAL ITEM NUMBER AND CONDITION ANNOTATION
-    resCondPattern=re.compile(r"0-(\d)")
+    resCondPattern=re.compile(r"(.*)-(.*)")
+    fillerCondPattern=re.compile(r"filler-(.*)")
     originalItemNum=0
     originalCondition=-1
     ResRowList[0].append("Original Item Number")
@@ -180,13 +193,15 @@ def WOI_Annotation(Exp,Res,Out):
     ResHeaderList.append("Condition")
     for idx, row in enumerate(ResRowList):
         #ITEM
-        if idx==0:
-            pass
-        elif idx!=0 and indexwd(row,ResHeaderList,"Group.")!="NULL":
-            originalItemNum=int(indexwd(row,ResHeaderList,"Group."))/2
-            ResRowList[idx].append(str(originalItemNum))
-        else:
-            ResRowList[idx].append("NULL")
+        #if idx==0:
+        #    pass
+        #elif idx!=0 and indexwd(row,ResHeaderList,"Group.")!="NULL":
+        #    originalItemNum=int(indexwd(row,ResHeaderList,"Group."))/2
+        #    ResRowList[idx].append(str(originalItemNum))
+        #elif idx!=0 and indexwd(row,ResHeaderList,"Group.")=="NULL":
+            #filler found, new pattern
+        #else:
+        #    ResRowList[idx].append("NULL")
         #CONDITION
         if idx==0:
             pass
@@ -195,12 +210,23 @@ def WOI_Annotation(Exp,Res,Out):
                 resCond=indexwd(row,ResHeaderList,"Type.")
             resMatch=resCondPattern.match(resCond)
             if resMatch:
+                if resMatch.group(1)=="filler":
+                    #filler
+                    originalCondition=resMatch.group(1)
+                    originalItemNum=resMatch.group(2)
+                else:
+                    #not filler
+                    originalCondition=resMatch.group(2)
+                    originalItemNum=resMatch.group(1)
                 originalCondition=resMatch.group(1)
             else:
                 originalCondition="NULL"
+                originalItemNum="NULL"
             #print "Original condition is: "+originalCondition
+            ResRowList[idx].append(str(originalItemNum))
             ResRowList[idx].append(str(originalCondition))
         else:
+            ResRowList[idx].append("NULL")
             ResRowList[idx].append("NULL")
     ###QUESTION COMBINATION(Word+Understanding)
     understandingRows=[]
@@ -213,7 +239,7 @@ def WOI_Annotation(Exp,Res,Out):
     for row in ResRowList[:]:
         #grab all understanding questions
         ##if it has a condition, an item number, a workerid!=null, and the word column isdigit()
-        if indexwd(row,ResHeaderList,"Field value./Word./Answer.").isdigit() and indexwd(row,ResHeaderList,"workerID")!="NULL"and indexwd(row,ResHeaderList,"Original Item Number")!="NULL" and indexwd(row,ResHeaderList,"Condition")!="NULL":
+        if indexwd(row,ResHeaderList,"Field value./Answer./Word.").isdigit() and indexwd(row,ResHeaderList,"workerID")!="NULL"and indexwd(row,ResHeaderList,"Original Item Number")!="NULL" and indexwd(row,ResHeaderList,"Condition")!="NULL":
             #store row
             understandingRows.append(row)
             #deleting from main list
@@ -245,12 +271,18 @@ def WOI_Annotation(Exp,Res,Out):
     ResHeaderList.append("Response Time")
     for idx, row in enumerate(ResRowList):
         for jdx, uRow in enumerate(understandingRows):
+            #print str(indexwd(row,ResHeaderList,"workerID"))+"=="+str(indexwd(uRow,ResHeaderList,"workerID"))
+            #print indexwd(row,ResHeaderList,"Original Item Number")+"=="+indexwd(uRow,ResHeaderList,"Original Item Number")
+            #print str(indexwd(row,ResHeaderList,"Condition"))+"=="+str(indexwd(uRow,ResHeaderList,"Condition"))
             if idx==0:
                 pass
             elif (indexwd(row,ResHeaderList,"workerID")==indexwd(uRow,ResHeaderList,"workerID")) and indexwd(row,ResHeaderList,"Original Item Number")==indexwd(uRow,ResHeaderList,"Original Item Number") and indexwd(row,ResHeaderList,"Condition")==indexwd(uRow,ResHeaderList,"Condition"):
-                ResRowList[idx].append(indexwd(uRow,ResHeaderList,"Field name./Question2 (NULL if none)./Word number./Question (NULL if none)."))
+                print "MATCH ITEM APPEND"
+                print idx
+                print jdx
+                ResRowList[idx].append(indexwd(uRow,ResHeaderList,"Field name./Question2 (NULL if none)./Question (NULL if none)./Word number."))
                 ResRowList[idx].append(indexwd(uRow,ResHeaderList,"Whether or not answer was correct (NULL if N/A)./Reading time."))
-                ResRowList[idx].append(indexwd(uRow,ResHeaderList,"Field value./Word./Answer."))
+                ResRowList[idx].append(indexwd(uRow,ResHeaderList,"Field value./Answer./Word."))
                 ResRowList[idx].append(indexwd(uRow,ResHeaderList,"Time taken to answer./Newline?"))
             else:
                 pass
@@ -258,6 +290,7 @@ def WOI_Annotation(Exp,Res,Out):
         if idx==0:
             pass
         else:
+            print "null fixing"
             #fill in nulls on non question columns
             #change to for loop, for each cell that needs to be filled(based on length)
             if len(row)<len(ResHeaderList):
@@ -271,7 +304,7 @@ def WOI_Annotation(Exp,Res,Out):
         if i==0:
             o.write('\t'.join(r)+'\n')
         else:
-            if indexwd(r,ResHeaderList,"Field name./Question2 (NULL if none)./Word number./Question (NULL if none).").isdigit():
+            if indexwd(r,ResHeaderList,"Field name./Question2 (NULL if none)./Question (NULL if none)./Word number.").isdigit():
                 o.write('\t'.join(r)+'\n')
             else:
                 pass
